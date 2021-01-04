95f22ba989684daf3ef31016a9663e997b1acf27
yeonkyujeon <ehb6915@naver.com>
2021-01-04 15:44:44 +0900

    Fix : bothTrueCheck -> roundUpCheck으로 변수명 수정

--- week1/binaryAdder/binaryAdder.js
+++ week1/binaryAdder/binaryAdder.js
@@ -2,22 +2,26 @@
 function halfAdder(bitA, bitB) {
   const answer = [];
   answer[0] = isBothTrue(bitA, bitB);
-  answer[1] = !isBothTrue(bitA,bitB) && (bitA || bitB);
+  answer[1] = !isBothTrue(bitA, bitB) && (bitA || bitB);
   return answer;
 }
 //전체 덧셈
 function fullAdder(bitA, bitB, carry) {
   const answer = [];
   const halfResult = halfAdder(bitA, bitB);
-  const bothTrueCheck = isBothTrue(halfResult[1], carry);
-  answer[0] = bothTrueCheck || halfResult[0];
-  answer[1] = !bothTrueCheck && (halfResult[1] || carry); 
+  const roundUpCheck = isBothTrue(halfResult[1], carry);
+  answer[0] = roundUpCheck || halfResult[0];
+  answer[1] = !roundUpCheck && (halfResult[1] || carry);
   return answer;
 }
 //둘다 참일때
-function isBothTrue(A, B){
+function isBothTrue(A, B) {
   return A && B;
 }
 
+function byteAdder(byteA, byteB) {
+  var answer = [];
+  return answer;
+}
 // console.log(halfadder(0, 0));
 console.log(fullAdder(0, 1, 0));
