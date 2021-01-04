3db81507dc2e18342af25c4034a83258a65d7ba2
yeonkyujeon <ehb6915@naver.com>
2021-01-04 21:55:38 +0900

    Refactor : 비트연산 and, xor은 따로 함수로 분리하지 않고 직접 사용하게 리팩토링
    (코드가 가독성이 더 떨어지는 느낌이었음)

--- week1/binaryAdder/binaryAdder.js
+++ week1/binaryAdder/binaryAdder.js
@@ -1,43 +1,33 @@
 
 function halfAdder(bitA, bitB) { //1비트를 더하는 함수
-  const answer = [];
-  answer[0] = isBothTrue(bitA, bitB);
-  answer[1] = getXor(bitA, bitB);
-  return answer;
+  return [bitA & bitB, bitA ^ bitB];
 }
 
 function fullAdder(bitA, bitB, carry) { //전체 덧셈
   const answer = [];
   const halfResult = halfAdder(bitA, bitB);
-  const roundUpCheck = isBothTrue(halfResult[1], carry);
-  answer[0] = roundUpCheck || halfResult[0];
-  answer[1] = getXor(halfResult[1], carry);
+  const roundUpCheck = halfResult[1] & carry;
+  answer[0] = roundUpCheck | halfResult[0];
+  answer[1] = halfResult[1] ^ carry;
   return answer;
-}
-
-function isBothTrue(A, B) { //둘다 참일때
-  return A && B;
-}
-
-function getXor(A, B) {
-  const result = !(A && B) && (A || B);
-  return result ? 1 : 0;
 }
 
 function byteAdder(byteA, byteB) { //8비트 덧셈기
   const answer = [];
   const maxLength = Math.max(byteA.length, byteB.length);
-  byteA = byteA.concat(new Array(maxLength - byteA.length).fill(0));
-  byteB = byteB.concat(new Array(maxLength - byteB.length).fill(0));
+  const cloneByteA = byteA.concat(new Array(maxLength - byteA.length).fill(0));
+  const cloneByteB = byteB.concat(new Array(maxLength - byteB.length).fill(0));
   let carry = 0;
   for (let i = 0; i < maxLength; i++) {
-    const [next, current] = fullAdder(byteA[i], byteB[i], carry);
+    const [next, current] = fullAdder(cloneByteA[i], cloneByteB[i], carry);
     answer[i] = current;
     carry = next;
   }
   answer[answer.length] = carry;
   return answer;
 }
+
+module.exports = { byteAdder };
 // console.log(halfadder(0, 0));
 // console.log(fullAdder(0, 1, 0));
 console.log(byteAdder([0, 1, 0, 1, 1, 0, 0, 0], [1, 0, 1, 1, 0, 0, 1, 1]));
