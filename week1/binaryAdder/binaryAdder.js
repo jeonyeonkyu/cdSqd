0b083b7e8d65c2ebcf609b9dcebc549ea0199432
yeonkyujeon <ehb6915@naver.com>
2021-01-04 16:44:30 +0900

    Feat : 8비트 덧셈기 함수 byteAdder() 구현

--- week1/binaryAdder/binaryAdder.js
+++ week1/binaryAdder/binaryAdder.js
@@ -1,7 +1,7 @@
 
 function halfAdder(bitA, bitB) { //1비트를 더하는 함수
   const answer = [];
-  answer[0] = (bitA && bitB);
+  answer[0] = isBothTrue(bitA, bitB);
   answer[1] = getXor(bitA, bitB);
   return answer;
 }
@@ -9,7 +9,8 @@ function halfAdder(bitA, bitB) { //1비트를 더하는 함수
 function fullAdder(bitA, bitB, carry) { //전체 덧셈
   const answer = [];
   const halfResult = halfAdder(bitA, bitB);
-  answer[0] = (halfResult[1] && carry) || halfResult[0];
+  const roundUpCheck = isBothTrue(halfResult[1], carry);
+  answer[0] = roundUpCheck || halfResult[0];
   answer[1] = getXor(halfResult[1], carry);
   return answer;
 }
@@ -22,6 +23,18 @@ function getXor(A, B) {
   return !(A && B) && (A || B);
 }
 
-
+function byteAdder(byteA, byteB) { //8비트 덧셈기
+  const answer = [];
+  let carry = 0;
+  for (let i = 0; i < byteA.length; i++) {
+    const [next, current] = fullAdder(byteA[i], byteB[i], carry);
+    answer[i] = current;
+    carry = next;
+  }
+  answer[answer.length] = carry;
+  return answer;
+}
 // console.log(halfadder(0, 0));
-console.log(fullAdder(0, 1, 1));
+// console.log(fullAdder(0, 1, 0));
+console.log(byteAdder([ 1, 1, 0, 1, 1, 0, 1, 0 ], [ 1, 0, 1, 1, 0, 0, 1, 1 ]));
+console.log(byteAdder([ 1, 1, 0, 0, 1, 0, 1, 0 ], [ 1, 1, 0, 1, 1, 0, 0, 1 ]));
