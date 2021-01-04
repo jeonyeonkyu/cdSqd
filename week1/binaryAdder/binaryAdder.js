e7c525900c21bc4bed4ae0566bc848cb2b925397
yeonkyujeon <ehb6915@naver.com>
2021-01-04 16:02:24 +0900

    Refactor : 중복되는 코드 getXor()함수로 리팩토링

--- week1/binaryAdder/binaryAdder.js
+++ week1/binaryAdder/binaryAdder.js
@@ -1,27 +1,27 @@
-//1비트를 더하는 함수
-function halfAdder(bitA, bitB) {
+
+function halfAdder(bitA, bitB) { //1비트를 더하는 함수
   const answer = [];
-  answer[0] = isBothTrue(bitA, bitB);
-  answer[1] = !isBothTrue(bitA, bitB) && (bitA || bitB);
+  answer[0] = (bitA && bitB);
+  answer[1] = getXor(bitA, bitB);
   return answer;
 }
-//전체 덧셈
-function fullAdder(bitA, bitB, carry) {
+
+function fullAdder(bitA, bitB, carry) { //전체 덧셈
   const answer = [];
   const halfResult = halfAdder(bitA, bitB);
-  const roundUpCheck = isBothTrue(halfResult[1], carry);
-  answer[0] = roundUpCheck || halfResult[0];
-  answer[1] = !roundUpCheck && (halfResult[1] || carry);
+  answer[0] = (halfResult[1] && carry) || halfResult[0];
+  answer[1] = getXor(halfResult[1], carry);
   return answer;
 }
-//둘다 참일때
-function isBothTrue(A, B) {
+
+function isBothTrue(A, B) { //둘다 참일때
   return A && B;
 }
 
-function byteAdder(byteA, byteB) {
-  var answer = [];
-  return answer;
+function getXor(A, B) {
+  return !(A && B) && (A || B);
 }
+
+
 // console.log(halfadder(0, 0));
-console.log(fullAdder(0, 1, 0));
+console.log(fullAdder(0, 1, 1));
