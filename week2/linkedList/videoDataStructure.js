192451191f0c1675a470b8937e7b11d88555a8b9
yeonkyujeon <ehb6915@naver.com>
2021-01-11 15:57:15 +0900

    Fix : 생성자 함수 class로 변경 후 MyVideo -> MyVideoData로 이름 변경

--- week2/linkedList/videoDataStructure.js
+++ week2/linkedList/videoDataStructure.js
@@ -1,44 +1,46 @@
 const uniqueIdGenerator = require('./UniqueIdGenerator.js');
 const titleGenerator = require('./TitleGenerator.js');
 
-const getPlayTime = () => {
-  return Math.ceil(Math.random() * 10) + Math.round(Math.random() * 5);
-}
+class MyVideoData {
+  constructor({ uniqueIdGenerator, titleGenerator }) {
+    this.id = uniqueIdGenerator.getUniqueId();
+    this.title = titleGenerator.getTitle();
+    this.playTime = this.getPlayTime();
+  }
 
-function MyVideo({ uniqueIdGenerator, titleGenerator }) {
-  this.id = uniqueIdGenerator.getUniqueId();
-  this.title = titleGenerator.getTitle();
-  this.playTime = getPlayTime();
+  getPlayTime() {
+    return Math.ceil(Math.random() * 10) + Math.round(Math.random() * 5);
+  }
 
-  this.printInformation = function () {
+  printInformation = function () {
     console.log(`${this.title}(${this.id}):${this.playTime}`);
   }
 }
 
-const myVideo1 = new MyVideo({ uniqueIdGenerator, titleGenerator });
-const myVideo2 = new MyVideo({ uniqueIdGenerator, titleGenerator });
-const myVideo3 = new MyVideo({ uniqueIdGenerator, titleGenerator });
-const myVideo4 = new MyVideo({ uniqueIdGenerator, titleGenerator });
-const myVideo5 = new MyVideo({ uniqueIdGenerator, titleGenerator });
-const myVideo6 = new MyVideo({ uniqueIdGenerator, titleGenerator });
-const myVideo7 = new MyVideo({ uniqueIdGenerator, titleGenerator });
-const myVideo8 = new MyVideo({ uniqueIdGenerator, titleGenerator });
-const myVideo9 = new MyVideo({ uniqueIdGenerator, titleGenerator });
-const myVideo10 = new MyVideo({ uniqueIdGenerator, titleGenerator });
-const myVideo11 = new MyVideo({ uniqueIdGenerator, titleGenerator });
-const myVideo12 = new MyVideo({ uniqueIdGenerator, titleGenerator });
-const myVideo13 = new MyVideo({ uniqueIdGenerator, titleGenerator });
+const myVideoData1 = new MyVideoData({ uniqueIdGenerator, titleGenerator });
+const myVideoData2 = new MyVideoData({ uniqueIdGenerator, titleGenerator });
+const myVideoData3 = new MyVideoData({ uniqueIdGenerator, titleGenerator });
+const myVideoData4 = new MyVideoData({ uniqueIdGenerator, titleGenerator });
+const myVideoData5 = new MyVideoData({ uniqueIdGenerator, titleGenerator });
+const myVideoData6 = new MyVideoData({ uniqueIdGenerator, titleGenerator });
+const myVideoData7 = new MyVideoData({ uniqueIdGenerator, titleGenerator });
+const myVideoData8 = new MyVideoData({ uniqueIdGenerator, titleGenerator });
+const myVideoData9 = new MyVideoData({ uniqueIdGenerator, titleGenerator });
+const myVideoData10 = new MyVideoData({ uniqueIdGenerator, titleGenerator });
+const myVideoData11 = new MyVideoData({ uniqueIdGenerator, titleGenerator });
+const myVideoData12 = new MyVideoData({ uniqueIdGenerator, titleGenerator });
+const myVideoData13 = new MyVideoData({ uniqueIdGenerator, titleGenerator });
 
-myVideo1.printInformation();
-myVideo2.printInformation();
-myVideo3.printInformation();
-myVideo4.printInformation();
-myVideo5.printInformation();
-myVideo6.printInformation();
-myVideo7.printInformation();
-myVideo8.printInformation();
-myVideo9.printInformation();
-myVideo10.printInformation();
-myVideo11.printInformation();
-myVideo12.printInformation();
-myVideo13.printInformation();
+myVideoData1.printInformation();
+myVideoData2.printInformation();
+myVideoData3.printInformation();
+myVideoData4.printInformation();
+myVideoData5.printInformation();
+myVideoData6.printInformation();
+myVideoData7.printInformation();
+myVideoData8.printInformation();
+myVideoData9.printInformation();
+myVideoData10.printInformation();
+myVideoData11.printInformation();
+myVideoData12.printInformation();
+myVideoData13.printInformation();
