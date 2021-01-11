e3637ef2e36ae6befef598d0344e6386834a399a
yeonkyujeon <ehb6915@naver.com>
2021-01-11 15:16:06 +0900

    Refactor : UniqueIdGenerator와 MyVideoList  모듈로 분리

--- week2/linkedList/videoDataStructure.js
+++ week2/linkedList/videoDataStructure.js
@@ -1,10 +1,14 @@
-class MyVideoList {
-  constructor() {
-    this.head = null;
-    this.next = null;
-  }
+const uniqueIdGenerator = require('./UniqueIdGenerator.js');
+
+function MyVideoList(uniqueIdGenerator) {
+  this.id = uniqueIdGenerator.getUniqueId();
+  this.head = null;
+  this.next = null;
 }
 
 const getPlayTime = () => {
   return Math.ceil(Math.random() * 10) + Math.round(Math.random() * 5);
 }
+
+const myVideoList = new MyVideoList(uniqueIdGenerator);
+console.log(myVideoList);
