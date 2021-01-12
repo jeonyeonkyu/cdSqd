eaf01ae092debdc8d0ac031fb7db353a65540da6
yeonkyujeon <ehb6915@naver.com>
2021-01-12 14:01:56 +0900

    Fix : 제목 13까지 하나씩 변수에 할당한것 for문 배열에 돌면서 넣기

--- week2/linkedList/videoDataStructure.js
+++ week2/linkedList/videoDataStructure.js
@@ -4,25 +4,10 @@ const MyVideoData = require('./MyVideoData.js');
 const videoListGenerator = require('./VideoListGenerator');
 const InputModule = require('./InputModule.js');
 
-const myVideoData1 = new MyVideoData({ uniqueIdGenerator, titleGenerator });
-const myVideoData2 = new MyVideoData({ uniqueIdGenerator, titleGenerator });
-const myVideoData3 = new MyVideoData({ uniqueIdGenerator, titleGenerator });
-const myVideoData4 = new MyVideoData({ uniqueIdGenerator, titleGenerator });
-const myVideoData5 = new MyVideoData({ uniqueIdGenerator, titleGenerator });
-const myVideoData6 = new MyVideoData({ uniqueIdGenerator, titleGenerator });
-const myVideoData7 = new MyVideoData({ uniqueIdGenerator, titleGenerator });
-const myVideoData8 = new MyVideoData({ uniqueIdGenerator, titleGenerator });
-const myVideoData9 = new MyVideoData({ uniqueIdGenerator, titleGenerator });
-const myVideoData10 = new MyVideoData({ uniqueIdGenerator, titleGenerator });
-const myVideoData11 = new MyVideoData({ uniqueIdGenerator, titleGenerator });
-const myVideoData12 = new MyVideoData({ uniqueIdGenerator, titleGenerator });
-const myVideoData13 = new MyVideoData({ uniqueIdGenerator, titleGenerator });
-
 const myVideoList = [];
-myVideoList.push(myVideoData1, myVideoData2, myVideoData3, myVideoData4,
-  myVideoData5, myVideoData6, myVideoData7, myVideoData8, myVideoData9,
-  myVideoData10, myVideoData11, myVideoData12, myVideoData13);
-
+for(let i = 0; i < 13; i++) {
+  myVideoList.push(new MyVideoData({ uniqueIdGenerator, titleGenerator }));
+}
 console.log('---영상 클립---');
 myVideoList.forEach(videoData => {
   videoData.printInformation();
