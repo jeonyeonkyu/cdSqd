bc4d718bf4e870dcf90491a1a496c0ceebe201a9
yeonkyujeon <ehb6915@naver.com>
2021-01-14 16:59:07 +0900

    Feat : input으로 들어온 글자를 숫자만 골라서 배열로 리턴하는 inputDivision() 함수 구현

--- week2/OOP/straightLength.js
+++ week2/OOP/straightLength.js
@@ -1,4 +1,7 @@
-class Location{
+class Location {
+  constructor() {
+    this.limitSize = 24;
+  }
   get x() {
     return this.xPosition;
   }
@@ -6,32 +9,53 @@ class Location{
     return this.yPosition;
   }
   set x(value) {
-    if(value > 24) {
-      throw new Error();
-    }
+    if (checkSize(value)) return -1;
     this.xPosition = value;
   }
   set y(value) {
-    if(value > 24) {
-      throw new Error();
-    }
+    if (checkSize(value)) return -1;
     this.yPosition = value;
+  }
+  checkSize(value) {
+    return value <= this.limitSize ? true : false;
   }
 }
 
-const useReadLine = () => {
+const inputDivision = (word) => {
+  const wordArray = word.split('-');
+  const aWord = wordArray[0].slice(1, wordArray[0].length - 1);
+  const bWord = wordArray[1].slice(1, wordArray[1].length - 1);
+  const A = aWord.split(',');
+  const B = bWord.split(',');
+  return [...A, ...B];
+}
+
+const useReadLine = (A, B) => {
   const readline = require('readline');
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
   });
+  console.log('> 좌표를 입력하세요.');
+  console.log('ex) (10,10)-(14,15)');
+  rl.setPrompt('> 잘못 입력하셨습니다. 다시 입력해주세요.');
+  rl.prompt();
   rl.on("line", (line) => {
-    rl.close();
+    [A.x, A.y, B.x, B.y] = inputDivision(line);
+    if (!checkNegative(A.x, A.y, B.x, B.y)){
+      
+    }
+
+      rl.close();
   })
   rl.on("close", () => {
     process.exit();
   })
 }
 
-const X = new Location();
-X.x = 24;
+
+const A = new Location();
+const B = new Location();
+// useReadLine(A, B);
+
+console.log(inputDivision('(10,10)-(14,15)'));
