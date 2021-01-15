9bf4379bcf7ed16290a33a093b43e78a358909fa
yeonkyujeon <ehb6915@naver.com>
2021-01-15 10:45:34 +0900

    Fix : extends가 필요없는 Square 클래스 수정

--- week2/OOP/Square.js
+++ week2/OOP/Square.js
@@ -1,6 +1,4 @@
-const Straight = require('./Straight.js');
-
-class Square extends Straight {
+class Square {
   getPointSavedSet(location) {
     return location.reduce(({ xSet, ySet }, { x, y }) => {
       xSet.add(x);
@@ -10,13 +8,13 @@ class Square extends Straight {
   }
 
   checkSquare(location) {
-    return Object.entries(super.getPointSavedSet(location))
+    return Object.entries(this.getPointSavedSet(location))
       .filter(([_, value]) => value.size === 2)
       .length === 2;
   }
 
   getSquareArea(location) {
-    const { xSet, ySet } = super.getPointSavedSet(location);
+    const { xSet, ySet } = this.getPointSavedSet(location);
     const [x1, x2] = [...xSet];
     const [y1, y2] = [...ySet];
     return Math.abs((x1 - x2) * (y1 - y2));
