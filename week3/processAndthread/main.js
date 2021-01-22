87243c8d7bde427f75a0ae8134c654ca9ce3b721
yeonkyujeon <ehb6915@naver.com>
2021-01-22 21:51:04 +0900

    Feat : 프로세스 요구사항 구현 완료
    worker가 비동기로 메시지를 보내서 오차가 살짝 있음

--- week3/processAndthread/main.js
+++ week3/processAndthread/main.js
@@ -7,8 +7,10 @@ const workerD = new Worker('./workerD.js');
 
 class Model {
   constructor(workerA, workerB, workerC, workerD) {
-    this.threadArray = [{ A: workerA }, { B: workerB }, { C: workerC }, { D: workerD }];
+    this.threadArray = [workerA, workerB, workerC, workerD];
     this.workStuff = { count: 1 };
+    this.waitStuff = { count: 0 };
+    this.workCount = -1;
   }
 
   cutRandomThreadArray(count) {
@@ -20,13 +22,23 @@ class Model {
     return [...this.threadArray];
   }
 
-  doWork(index) {
-    this.threadArray[index][Object.keys(this.threadArray[index])].postMessage(this.workStuff);
+  getReady() {
+    this.threadArray[0].postMessage(this.waitStuff);
+    this.threadArray[1].postMessage(this.waitStuff);
+    this.threadArray[2].postMessage(this.waitStuff);
   }
 
-  getWorkersMessage() {
+  getWaiting(index) {
+    this.threadArray[index].postMessage(this.waitStuff);
+  }
+
+  doWork(index) {
+    this.threadArray[index].postMessage(this.workStuff);
+  }
+
+  getWorkersMessage(index) {
     return new Promise(resolve => {
-      this.threadArray[index][Object.keys(this.threadArray[index])].on('message', resolve);
+      this.threadArray[index].on('message', resolve);
     });
   }
 
@@ -36,16 +48,76 @@ class Model {
 class View {
   constructor(model) {
     this.model = model;
+    this.finishSet = new Set();
+  }
+
+  init() {
+    this.model.getReady();
+    this.printFormatWorker(0);
+    this.printFormatWorker(1);
+    this.printFormatWorker(2);
+    this.render();
   }
 
   render() {
+    const self = this;
+    let timeId = setTimeout(function tick() {
+      if (self.finishSet.size === 3) {
+        console.log('프로세스 종료');
+        process.exit();
+        return;
+      }
+      const index = self.getIndexWithWork();
+      console.log('.');
+      self.model.doWork(index);
+      self.printFormatWorker(index);
 
+      if (index === 0) {
+        self.model.getWaiting(1);
+        self.printFormatWorker(1);
+        self.model.getWaiting(2);
+        self.printFormatWorker(2);
+      } else if (index === 1) {
+        self.model.getWaiting(0);
+        self.printFormatWorker(0);
+        self.model.getWaiting(2);
+        self.printFormatWorker(2);
+      } else if (index === 2) {
+        self.model.getWaiting(0);
+        self.printFormatWorker(0);
+        self.model.getWaiting(1);
+        self.printFormatWorker(1);
+      }
+
+      timeId = setTimeout(tick, 1000);
+    }, 1000)
+  }
+
+  printFormatWorker(index) {
+    this.model.getWorkersMessage(index).then(v => {
+      const [name, state, count, target] = v.split(',');
+      console.log(`${name}(${state}), ${count} / ${target}sec`);
+
+      if (state === 'terminate') {
+        this.finishSet.add(name);
+      }
+    });
+  }
+
+  getIndexWithWork() {
+    let index = ++this.model.workCount % 3;
+    if (this.finishSet.size === 1) {
+      index = this.model.workCount % 2 + 1
+    } else if (this.finishSet.size === 2) {
+      index = 2;
+    }
+    return index;
   }
 }
 
 const model = new Model(workerA, workerB, workerC, workerD);
-// model.cutRandomThreadArray(3);
-// console.log(model.getThreadArray())
+model.cutRandomThreadArray(3);
 
-model.doWork();
-// process.exit()
+const view = new View(model);
+view.init();
+
