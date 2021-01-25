2fb916ded48c332c6a3dc236fdd3ff81a0032f7f
yeonkyujeon <ehb6915@naver.com>
2021-01-25 17:37:50 +0900

    Add : cafeOrder 미션 구현하기 위한 틀

---
+++ week4/cafeOrder/cafeOrder.js
@@ -0,0 +1,49 @@
+//주문 캐셔에게 이동
+//캐셔 받은 주문을 주문 대기표에 추가 
+//매니저 주문 대기표를 1초마다 확인, 바리스타의 음료제작상태 확인 후 전달 1초에 끝남과 동시에 바리스타에게 전달해야될듯
+//바리스타 동시에 2개까지 음료 만들수 잇음 음료 만들기 시작, 음료만들기 끝 에대한 이벤트
+//현황판 음료가 대기중인지 제작중인지 완료인지 출력 < 매니저가 확인해야할듯
+
+// const EventEmitter = require('events');
+// const emitter = new EventEmitter();
+
+// let customerNumber = 0;
+// emitter.on('message', (msg, msg2) => {
+//   console.log('casll');
+//   console.log(msg)
+//   console.log(msg2)
+// })
+// emitter.emit('message', ++customerNumber, '1:3');
+
+class Cashier {
+  constructor() {
+
+  }
+}
+
+class Manager {
+  constructor() {
+
+  }
+}
+
+class DashBoard {
+  constructor() {
+
+  }
+}
+
+class Barista {
+  constructor() {
+
+  }
+}
+
+class OrderModule {
+  constructor() {
+
+  }
+}
+
+
+
