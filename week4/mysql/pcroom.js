969a2a2da7a0eb3f0356e308f106c97c73065062
yeonkyujeon <ehb6915@naver.com>
2021-01-30 01:57:11 +0900

    Refactor : 함수들 class로 따로 분리

--- week4/mysql/pcroom.js
+++ week4/mysql/pcroom.js
@@ -1,59 +1,54 @@
 const db = require('./dbConnection.js');
 // db.connect();
 
+class PcRoom {
+  runMysql(sql) {
+    return new Promise((resolve, reject) => {
+      db.query(sql, function (error, results) {
+        if (error) reject(error);
+        resolve(results);
+      });
+    })
+  }
 
-const runMysql = (sql) => {
-  return new Promise((resolve, reject) => {
-    db.query(sql, function (error, results, fieldList) {
-      if (error) reject(error);
-      resolve(results);
-    });
-  })
+  getEmptySeats = async () => {
+    const seatPacket = await this.runMysql('SELECT seatnumber from PCSEAT where in_use = 0');
+    const seatArray = seatPacket.map(e => e.seatnumber);
+    return seatArray;
+  }
+
+  getRandomSeats = async () => {
+    const seats = await this.getEmptySeats();
+    const randomSeatNumber = Math.floor(Math.random() * seats.length);
+    return randomSeatNumber;
+  }
+
+  insertNewUser = async (nickname, seatNumber) => {
+    await this.runMysql(`insert into PCROOM (nickname, seatnumber) values ('${nickname}', ${seatNumber})`);
+    await this.runMysql(`update PCSEAT set in_use = '1' where seatnumber = ${seatNumber}`);
+  }
+
+  getUserSeat = async (nickname) => {
+    return await this.runMysql(`select seatnumber from PCROOM where nickname = '${nickname}'`);
+  }
+
+  getNowDate() {
+    const date = new Date();
+    const [year, month, day] = [date.getFullYear(), date.getMonth() + 1, date.getDate()];
+    const [hours, minutes, seconds] = [date.getHours(), date.getMinutes(), date.getSeconds()];
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  }
+
+  quitUser = async (nickname) => {
+    const userSeat = await this.getUserSeat();
+    await this.runMysql(`update PCSEAT set in_use = '0' where seatnumber = ${userSeat}`);
+    await this.runMysql(`update PCROOM set endtime = '${this.getNowDate()}' where nickname = '${nickname}'`);
+  }
+
+  getLastUserKey = async () => {
+    return await this.runMysql(`select MAX(id) from PCROOM`);
+  }
 }
-
-const asyncGetEmptySeats = async () => {
-  const seatPacket = await runMysql('SELECT seatnumber from PCSEAT where in_use = 0');
-  const seatArray = seatPacket.map(e => e.seatnumber);
-  return seatArray;
-}
-
-const getRandomSeats = async () => {
-  const seats = await asyncGetEmptySeats();
-  const randomSeatNumber = Math.floor(Math.random() * seats.length);
-  return randomSeatNumber;
-}
-
-const insertNewUser = async (nickname, seatNumber) => {
-  await runMysql(`insert into PCROOM (nickname, seatnumber) values ('${nickname}', ${seatNumber})`);
-  await runMysql(`update PCSEAT set in_use = '1' where seatnumber = ${seatNumber}`);
-}
-
-const getUserSeat = async (nickname) => {
-  return await runMysql(`select seatnumber from PCROOM where nickname = '${nickname}'`);
-}
-
-const getNowDate = () => {
-  const date = new Date();
-  const [year, month, day] = [date.getFullYear(), date.getMonth() + 1, date.getDate()];
-  const [hours, minutes, seconds] = [date.getHours(), date.getMinutes(), date.getSeconds()];
-  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-}
-
-const quitUser = async (nickname) => {
-  const userSeat = await getUserSeat();
-  await runMysql(`update PCSEAT set in_use = '0' where seatnumber = ${userSeat}`);
-  await runMysql(`update PCROOM set endtime = '${getNowDate()}' where nickname = '${nickname}'`);
-}
-
-const getLastUserKey = async () => {
-  return await runMysql(`select MAX(id) from PCROOM`);
-}
-
-// insertNewUser('aaB');
-quitUser('new');
-
-console.log(getNowDate())
-
 
 class PcModule {
   constructor() {
@@ -81,9 +76,8 @@ class PcModule {
 
   closeModule() { //모듈 종료
     this.rl.on("close", () => {
+      db.end();
       process.exit();
     })
   }
 }
-
-// db.end();
