fe89cc4e739fcd3e113d0bd254b7b1b1571ccafb
yeonkyujeon <ehb6915@naver.com>
2021-01-30 00:47:29 +0900

    Feat : runMysql, asyncGetEmptySeats, getRandomSeats, insertNewUser, getNowDate 구현

--- week4/mysql/pcroom.js
+++ week4/mysql/pcroom.js
@@ -1,17 +1,78 @@
-const mysql = require('mysql');
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '1004',
-  database: 'study_db'
-});
-
-connection.connect();
-
-connection.query('SELECT * from PCSEAT', function (error, results, fields) {
-  if (error) throw error;
-  console.log(results);
-});
+const db = require('./dbConnection.js');
+// db.connect();
 
 
-connection.end();
+const runMysql = (sql) => {
+  return new Promise((resolve, reject) => {
+    db.query(sql, function (error, results, fieldList) {
+      if (error) reject(error);
+      resolve(results);
+    });
+  })
+}
+
+const asyncGetEmptySeats = async () => {
+  const seatPacket = await runMysql('SELECT seatnumber from PCSEAT where in_use = 0');
+  const seatArray = seatPacket.map(e => e.seatnumber);
+  return seatArray;
+}
+
+const getRandomSeats = async () => {
+  const seats = await asyncGetEmptySeats();
+  const randomSeatNumber = Math.floor(Math.random() * seats.length);
+  return randomSeatNumber;
+}
+
+const insertNewUser = async (nickname, seatNumber) => {
+  await runMysql(`insert into PCROOM (nickname, seatnumber) values ('${nickname}', ${seatNumber})`);
+  await runMysql(`update PCSEAT set in_use = '1' where seatnumber = ${seatNumber}`);
+}
+
+const quitUser = async (nickname) => {
+  await runMysql(`update PCROOM set endtime = '${getNowDate()}' where nickname = '${nickname}'`);
+}
+
+const getNowDate = () => {
+  const date = new Date();
+  const [year, month, day] = [date.getFullYear(), date.getMonth() + 1, date.getDate()];
+  const [hours, minutes, seconds] = [date.getHours(), date.getMinutes(), date.getSeconds()];
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
+// insertNewUser('aaB');
+quitUser('new');
+
+console.log(getNowDate())
+
+
+class PcModule {
+  constructor() {
+    this.readline = require('readline');
+    this.rl = this.readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+      prompt: '입력> '
+    });
+  }
+
+  init() {
+    this.rl.prompt();
+    this.receiveInput();
+    this.closeModule();
+  }
+
+  receiveInput() { //입력받은 문자열 읽기
+    this.rl.on("line", (line) => {
+      if (line === 'quit') {
+        this.rl.close();
+      }
+    })
+  }
+
+  closeModule() { //모듈 종료
+    this.rl.on("close", () => {
+      process.exit();
+    })
+  }
+}
+
+// db.end();
