78406504feef7f27b3e5923666c3a7efa98b7376
yeonkyujeon <ehb6915@naver.com>
2021-02-03 00:07:07 +0900

    Feat : request, response header, response body 데이터 포맷에 맞춰서 출력하기 완료
    미션2 완료 , HttpResponse, run 함수 구현

--- week5/http/httpRequest.js
+++ week5/http/httpRequest.js
@@ -6,7 +6,6 @@ class HttpRequest {
   constructor(method = 'GET') {
     this.method = method;
   }
-
   getMessage(address) {
     return `${this.method} / HTTP/1.1\r\nHost: ${address}\r\n\r\n`;
   }
@@ -20,38 +19,67 @@ class Socket {
     this.request = request;
   }
 
-  init() {
-    this.connect();
-    this.takeData();
-  }
-
   connect() {
     this.socket.connect({ port: this.port, host: this.address }, () => {
       log('(DNS Lookup...)');
       log(`TCP Connection: ${this.address} 80`);
+      this.sendMessage();
+      this.printRequestMessage();
     })
-    this.write();
   }
 
-  write() {
+  sendMessage() {
     this.socket.write(`${this.request.getMessage(this.address)}`);
   }
 
   takeData() {
-    this.socket.on('data', (data) => {
-      console.log(data.toString());
+    return new Promise(resolve => {
+      this.socket.on('data', (data) => {
+        resolve(data.toString().split('\r\n\r\n'));
+      })
     })
+  }
+
+  quit() {
+    this.socket.end();
+  }
+
+  printRequestMessage() {
+    log('---------------------------------------');
+    log('|             HTTP REQUEST            |');
+    log('---------------------------------------');
+    log(this.request.getMessage(this.address));
+  }
+}
+
+class HttpResponse {
+  constructor(responseData) {
+    this.header = responseData[0];
+    this.body = responseData[1];
+  }
+
+  printResponseMessage() {
+    log('---------------------------------------');
+    log('|         HTTP RESPONSE HEADER        |');
+    log('---------------------------------------');
+    log(this.header);
+    log('---------------------------------------');
+    log('|          HTTP RESPONSE BODY         |');
+    log('---------------------------------------');
+    log(this.body);
   }
 }
 
 const run = (url) => {
-  dns.lookup(url, (err, address) => {
+  dns.lookup(url, async (err, address) => {
     if (err) throw err;
     const request = new HttpRequest();
     const socket = new Socket(address, 80, request);
-    socket.init();
+    socket.connect();
+    const response = new HttpResponse(await socket.takeData());
+    response.printResponseMessage();
+    socket.quit();
   });
 }
 
 run('www.kyobobook.co.kr');
-
