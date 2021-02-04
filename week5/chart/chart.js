128c58861db609b3291145959bf47282cd0b9922
yeonkyujeon <ehb6915@naver.com>
2021-02-05 01:43:47 +0900

    Fix : dataAdaptor 수정

--- week5/chart/chart.js
+++ week5/chart/chart.js
@@ -3,6 +3,7 @@ const inputFile = document.getElementById('csv_file');
 inputFile.addEventListener('change', async (event) => {
   const data = await loadFile(event.target);
   const model = new Model(dataAdaptor(data));
+  console.log(model.getDataLength(model.people));
 })
 
 function loadFile(sender) {
@@ -17,12 +18,24 @@ function loadFile(sender) {
   })
 }
 
-function dataAdaptor(data) {
-  return data.slice(2).map((item) => item.split(','));
+function dataAdaptor(peopleData) {
+  const title = peopleData[0].replace(' ', '').split(',');
+  return peopleData.slice(2).map((item) => {
+    const personData = item.split(',');
+    const person = {};
+    for (let i = 0; i < title.length; i++) {
+      person[title[i]] = personData[i];
+    }
+    return person;
+  });
 }
 
 class Model {
-  constructor(data) {
+  constructor(people) {
+    this.people = people;
+  }
 
+  getDataLength(people) {
+    return Object.keys(people[0]).length;
   }
 }
