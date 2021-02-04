7820e20d08d4f948d85d6896efafca65c51c410a
yeonkyujeon <ehb6915@naver.com>
2021-02-04 16:40:43 +0900

    Feat : canvas로 졸라맨 그리기 구현

--- week5/canvas/draw.js
+++ week5/canvas/draw.js
@@ -1,5 +1,34 @@
 const canvas = document.getElementById('small_man');
 const ctx = canvas.getContext('2d');
 
-ctx.fillStyle = '#197291';
-ctx.fillRect(30, 30, 100, 100);
+ctx.beginPath();
+ctx.fillStyle = '#ffffff';
+ctx.arc(140, 120, 70, 0, Math.PI * 2);
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.fillStyle = '#000000'
+ctx.arc(180, 90, 8, 0, Math.PI * 2);
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.fillStyle = '#000000'
+ctx.arc(140, 120, 70, 50, 13.2);
+ctx.lineTo(140, 120);
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.lineWidth = 4;
+ctx.moveTo(80, 400);
+ctx.lineTo(140, 260);
+ctx.lineTo(200, 400);
+ctx.stroke();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.fillStyle = '#ffffff';
+ctx.fillRect(70, 190, 140, 120);
+ctx.closePath();
