9d64e3cdd7e1462cb8a4768a11256651397de568
yeonkyujeon <ehb6915@naver.com>
2021-02-05 17:33:31 +0900

    Feat : Model에서 기타데이터 etc 프로퍼티로 포맷해주는 etcFormatter함수 구현

--- week5/chart/chart.js
+++ week5/chart/chart.js
@@ -3,8 +3,9 @@ const inputFile = document.getElementById('csv_file');
 inputFile.addEventListener('change', async (event) => {
   const data = await loadFile(event.target);
   const peopleList = dataAdaptor(data).map(person => new People(person));
+  // console.log(peopleList)
   const model = new Model(peopleList);
-  console.log(model.getDividedCategoryCount(model.people, 'game'))
+  console.log(model.etcFormatter(model.getDividedCategoryCount(model.people, 'game')))
 })
 
 function loadFile(sender) {
@@ -20,12 +21,25 @@ function loadFile(sender) {
 }
 
 function dataAdaptor(peopleData) {
-  return peopleData.slice(2).map((item) => item.split(','));
+  return peopleData.slice(2).map((item) => item.split(',').map(e => e = e || 'null'));
+}
+
+class People {
+  constructor(person) {
+    this.name = person[0];
+    this.age = person[1];
+    this.fruit = person[2];
+    this.game = person[3];
+  }
 }
 
 class Model {
   constructor(people) {
     this.people = people;
+    this.dataCount = people.length;
+    this.ageList = this.etcFormatter(this.getDividedAgeCount(people));
+    this.fruitList = this.etcFormatter(this.getDividedCategoryCount(people, 'fruit'));
+    this.gameList = this.etcFormatter(this.getDividedCategoryCount(people, 'game'));
   }
 
   getDividedAgeCount(people) {
@@ -47,13 +61,27 @@ class Model {
     }, {});
   }
 
+  //제일 많은 데이터 5개를 제외한 데이터는 기타(etc)로 바꿔서 포맷
+  etcFormatter(categoryCountData) {
+    const categoryEntries = Object.entries(categoryCountData).sort((a, b) => b[1] - a[1]);
+    const toBeEnteredData = categoryEntries.filter((_, i) => i <= 4)
+      .reduce((acc, cur) => {
+        acc[cur[0]] = cur[1];
+        return acc;
+      }, {});
+    const etcData = categoryEntries.filter((_, i) => i > 4)
+      .reduce((acc, cur) => {
+        acc['etc'] = cur[1];
+        return acc;
+      }, {});
+    return { ...toBeEnteredData, ...etcData };
+  }
 }
 
-class People {
-  constructor(person) {
-    this.name = person[0];
-    this.age = person[1];
-    this.fruit = person[2];
-    this.game = person[3];
+class View {
+  colors = ['#ff6b6b', '#cc5de8', '#845ef7', '#22b8cf', '#20c997', '#ff922b'];
+  constructor(model) {
+    this.model = model;
   }
+
 }
